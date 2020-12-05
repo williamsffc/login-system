b2afc98507928e1226bd5f4f25a06c889c8e46f9
Will <williamsffc@gmail.com>
2020-12-04 22:10:58 -0800

    created routes folder and app.js

---
+++ app.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const router = express.Router();
+const app = express();
+const mongoose = require("mongoose");
+const expressEjsLayout = require("express-ejs-layouts");
+
+//Mongoose
+mongoose
+  .connect("mongodb://localhost/test", {
+    userNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("connected,,"))
+  .catch((err) => console.log(err));
+
+//EJS
+app.set("view engine", "ejs");
+app.use(expressEjsLayout);
+
+//BodyParser
+app.use(express.urlencoded({ extended: false }));
+
+//Routes
+app.use("/", require("./routes/index"));
+app.use("/users", require("./routes/users"));
+
+app.listen(3000);
---
+++ routes/users.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const { route } = require(".");
+const router = express.Router();
+
+//Login handle
+router.get("/login", (req, res) => {
+  res.render("login");
+});
+
+router.get("/register", (req, res) => {
+  res.render("register");
+});
+
+//Register handle
+router.post("/register", (req, res) => {});
+
+router.post("/login", (req, res, next) => {});
+
+//Logout
+router.get("/logout", (req, res) => {});
+
+module.exports = router;
